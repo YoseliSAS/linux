50d41f3f94087f806524a9d44314c2997ebb9f82
Jean-Michel Hautbois <jeanmichel.hautbois@yoseli.org>
2024-10-02 09:46:16 +0200

    m68k: m54418: Add support for L2 ethernet switch
    This patch introduces the L2 switch support for M54418. It is based on
    the old BSP driver and a few APIs have been changed.
    
    Signed-off-by: Jean-Michel Hautbois <jeanmichel.hautbois@yoseli.org>

--- include/linux/fsl_devices.h
+++ include/linux/fsl_devices.h
@@ -152,4 +152,21 @@ int fsl_deep_sleep(void);
 static inline int fsl_deep_sleep(void) { return 0; }
 #endif
 
+struct net_device;
+struct coldfire_switch_platform_data {
+	int     hash_table;
+	unsigned int *switch_hw;
+	void    (*request_intrs)(struct net_device *dev,
+		irqreturn_t (*)(int, void *),
+		void *irq_privatedata);
+	void    (*set_mii)(struct net_device *dev);
+	void    (*get_mac)(struct net_device *dev);
+	void    (*enable_phy_intr)(void);
+	void    (*disable_phy_intr)(void);
+	void    (*phy_ack_intr)(void);
+	void    (*localhw_setup)(void);
+	void    (*uncache)(unsigned long addr);
+	void    (*platform_flush_cache)(void);
+};
+
 #endif /* _FSL_DEVICE_H_ */
