8da741396b34ec34732fb1f6c1f8806e24f96dd4
Jean-Michel Hautbois <jeanmichel.hautbois@yoseli.org>
2024-10-26 11:46:56 +0200

    DNI: first steap twoard DMA support for mcf uart
    Signed-off-by: Jean-Michel Hautbois <jeanmichel.hautbois@yoseli.org>

--- drivers/tty/serial/mcf.c
+++ drivers/tty/serial/mcf.c
@@ -9,6 +9,7 @@
 
 /****************************************************************************/
 
+#include <linux/delay.h>
 #include <linux/kernel.h>
 #include <linux/init.h>
 #include <linux/interrupt.h>
@@ -25,6 +26,10 @@
 #include <asm/mcfsim.h>
 #include <asm/mcfuart.h>
 #include <asm/nettel.h>
+
+#include <linux/circ_buf.h>
+#include <linux/dmaengine.h>
+#include <linux/dma-mapping.h>
 
 /****************************************************************************/
 
@@ -53,7 +58,28 @@ struct mcf_uart {
 	struct uart_port	port;
 	unsigned int		sigs;		/* Local copy of line sigs */
 	unsigned char		imr;		/* Local IMR mirror */
+
+	/* DMA fields */
+	unsigned int		dma_is_enabled:1;
+	unsigned int		dma_is_rxing:1;
+	unsigned int		dma_is_txing:1;
+	struct dma_chan		*dma_chan_rx, *dma_chan_tx;
+	struct scatterlist	rx_sgl, tx_sgl[2];
+	void			*rx_buf;
+	struct circ_buf		rx_ring;
+	unsigned int		rx_buf_size;
+	unsigned int		rx_period_length;
+	unsigned int		rx_periods;
+	dma_cookie_t		rx_cookie;
+	unsigned int		tx_bytes;
+	unsigned int		dma_tx_nents;
+	unsigned int            saved_reg[10];
+	bool			context_saved;
+	unsigned int		dma_chan_id;	/* DMA channel to request */
+	unsigned char		*dma_buf;	/* SW-FIFO (DMA destination) */
 };
+
+#define MCFUART_DMA_RXFIFOSIZE	128
 
 /****************************************************************************/
 
@@ -145,6 +171,118 @@ static void mcf_break_ctl(struct uart_port *port, int break_state)
 
 /****************************************************************************/
 
+static void mcf_uart_dma_rx_callback(void *data)
+{
+	struct mcf_uart *sport = data;
+	struct uart_port *port = &sport->port;
+	struct dma_chan	*chan = sport->dma_chan_rx;
+	struct scatterlist *sgl = &sport->rx_sgl;
+	struct dma_tx_state state;
+	struct circ_buf *rx_ring = &sport->rx_ring;
+	enum dma_status status;
+	unsigned int w_bytes = 0;
+	unsigned int r_bytes;
+	unsigned int bd_size;
+
+	status = dmaengine_tx_status(chan, sport->rx_cookie, &state);
+
+	if (status == DMA_ERROR) {
+		uart_port_lock(&sport->port);
+		//mcf_uart_clear_rx_errors(sport);
+		dev_err(port->dev, "DMA error !\n");
+		uart_port_unlock(&sport->port);
+		return;
+	}
+
+	trace_printk("We could get a DMA byte now !\n");
+}
+
+/* Default RX DMA buffer configuration */
+#define RX_DMA_PERIODS		16
+#define RX_DMA_PERIOD_LEN	(PAGE_SIZE / 4)
+
+static int mcf_dma_setup(struct uart_port *port)
+{
+	struct mcf_uart *pp = container_of(port, struct mcf_uart, port);
+	struct dma_chan *chan;
+	struct dma_slave_config slave_config = {};
+	int ret;
+
+	/* Prepare for RX : */
+	chan = dma_request_chan(port->dev, "rx");
+	if (IS_ERR(chan)) {
+		pp->dma_chan_rx = NULL;
+		ret = PTR_ERR(chan);
+		dev_err(port->dev, "cannot get the DMA channel: %d\n", ret);
+		goto err;
+	}
+	pp->dma_chan_rx = chan;
+	printk("%s: Chan %d requested\n", __func__, chan->chan_id);
+
+	slave_config.direction = DMA_DEV_TO_MEM;
+	slave_config.src_addr = (phys_addr_t) (port->membase + MCFUART_URB);
+	slave_config.src_addr_width = DMA_SLAVE_BUSWIDTH_1_BYTE;
+
+	ret = dmaengine_slave_config(pp->dma_chan_rx, &slave_config);
+	if (ret) {
+		dev_err(port->dev, "error in RX dma configuration.\n");
+		goto err;
+	}
+
+	pp->rx_period_length = RX_DMA_PERIOD_LEN;
+	pp->rx_periods = RX_DMA_PERIODS;
+	pp->rx_buf_size = pp->rx_period_length * pp->rx_periods;
+	pp->rx_buf = devm_kzalloc(port->dev, pp->rx_buf_size, GFP_KERNEL);
+	if (!pp->rx_buf) {
+		ret = -ENOMEM;
+		goto err;
+	}
+	pp->rx_ring.buf = pp->rx_buf;
+
+err:
+	return ret;
+}
+
+static int mcf_start_rx_dma(struct uart_port *port)
+{
+	struct mcf_uart *sport = container_of(port, struct mcf_uart, port);
+	struct scatterlist *sgl = &sport->rx_sgl;
+	struct dma_chan	*chan = sport->dma_chan_rx;
+	struct device *dev = port->dev;
+	struct dma_async_tx_descriptor *desc;
+	int ret;
+
+	dev_err(dev, "We are on line %dfor device %s\n", port->port_id, dev_name(dev));
+	sport->rx_ring.head = 0;
+	sport->rx_ring.tail = 0;
+
+	sg_init_one(sgl, sport->rx_buf, sport->rx_buf_size);
+	ret = dma_map_sg(dev, sgl, 1, DMA_FROM_DEVICE);
+	if (ret == 0) {
+		dev_err(dev, "DMA mapping error for RX.\n");
+		return -EINVAL;
+	}
+
+	desc = dmaengine_prep_dma_cyclic(chan, sg_dma_address(sgl),
+		sg_dma_len(sgl), sg_dma_len(sgl) / sport->rx_periods,
+		DMA_DEV_TO_MEM, DMA_PREP_INTERRUPT);
+
+	if (!desc) {
+		dma_unmap_sg(dev, sgl, 1, DMA_FROM_DEVICE);
+		dev_err(dev, "We cannot prepare for the RX slave dma!\n");
+		return -EINVAL;
+	}
+	desc->callback = mcf_uart_dma_rx_callback;
+	desc->callback_param = sport;
+
+	dev_dbg(dev, "RX: prepare for the DMA.\n");
+	sport->dma_is_rxing = 1;
+	sport->rx_cookie = dmaengine_submit(desc);
+	dma_async_issue_pending(chan);
+	return 0;
+}
+
+
 static int mcf_startup(struct uart_port *port)
 {
 	struct mcf_uart *pp = container_of(port, struct mcf_uart, port);
@@ -163,6 +301,9 @@ static int mcf_startup(struct uart_port *port)
 	/* Enable RX interrupts now */
 	pp->imr = MCFUART_UIR_RXREADY;
 	writeb(pp->imr, port->membase + MCFUART_UIMR);
+
+	if (port->port_id == 2)
+		mcf_start_rx_dma(port);
 
 	uart_port_unlock_irqrestore(port, flags);
 
@@ -278,50 +419,164 @@ static void mcf_set_termios(struct uart_port *port, struct ktermios *termios,
 
 /****************************************************************************/
 
+static inline void mcf_rx_one_char(struct uart_port *port,
+				   unsigned char     status,
+				   unsigned char     ch)
+{
+	unsigned char flag;
+
+	flag = TTY_NORMAL;
+	if (status & MCFUART_USR_RXERR) {
+		writeb(MCFUART_UCR_CMDRESETERR,
+			port->membase + MCFUART_UCR);
+
+		if (status & MCFUART_USR_RXBREAK) {
+			port->icount.brk++;
+			if (uart_handle_break(port))
+				return;
+		} else if (status & MCFUART_USR_RXPARITY) {
+			port->icount.parity++;
+		} else if (status & MCFUART_USR_RXOVERRUN) {
+			port->icount.overrun++;
+		} else if (status & MCFUART_USR_RXFRAMING) {
+			port->icount.frame++;
+		}
+
+		status &= port->read_status_mask;
+
+		if (status & MCFUART_USR_RXBREAK)
+			flag = TTY_BREAK;
+		else if (status & MCFUART_USR_RXPARITY)
+			flag = TTY_PARITY;
+		else if (status & MCFUART_USR_RXFRAMING)
+			flag = TTY_FRAME;
+	}
+
+	if (uart_handle_sysrq_char(port, ch))
+		return;
+	uart_insert_char(port, status, MCFUART_USR_RXOVERRUN, ch, flag);
+}
+
 static void mcf_rx_chars(struct mcf_uart *pp)
 {
 	struct uart_port *port = &pp->port;
-	u8 status, ch, flag;
+	u8 status, ch;
 
 	while ((status = readb(port->membase + MCFUART_USR)) & MCFUART_USR_RXREADY) {
 		ch = readb(port->membase + MCFUART_URB);
-		flag = TTY_NORMAL;
 		port->icount.rx++;
 
-		if (status & MCFUART_USR_RXERR) {
-			writeb(MCFUART_UCR_CMDRESETERR,
-				port->membase + MCFUART_UCR);
-
-			if (status & MCFUART_USR_RXBREAK) {
-				port->icount.brk++;
-				if (uart_handle_break(port))
-					continue;
-			} else if (status & MCFUART_USR_RXPARITY) {
-				port->icount.parity++;
-			} else if (status & MCFUART_USR_RXOVERRUN) {
-				port->icount.overrun++;
-			} else if (status & MCFUART_USR_RXFRAMING) {
-				port->icount.frame++;
-			}
-
-			status &= port->read_status_mask;
-
-			if (status & MCFUART_USR_RXBREAK)
-				flag = TTY_BREAK;
-			else if (status & MCFUART_USR_RXPARITY)
-				flag = TTY_PARITY;
-			else if (status & MCFUART_USR_RXFRAMING)
-				flag = TTY_FRAME;
-		}
-
-		if (uart_handle_sysrq_char(port, ch))
-			continue;
-		uart_insert_char(port, status, MCFUART_USR_RXOVERRUN, ch, flag);
+		mcf_rx_one_char(port, status, ch);
 	}
 
 	tty_flip_buffer_push(&port->state->port);
 }
 
+#if 1
+
+static void mcf_rx_handler(struct mcf_uart *pp)
+{
+	struct uart_port *port = &pp->port;
+	int count = 0;
+	u8 status;
+	unsigned char flag;
+	struct char_buf {
+		u8 ch;
+		u8 flag;
+		u8 status;
+	};
+	struct char_buf buf[256];
+
+	status = readb(port->membase + MCFUART_USR);
+	do {
+		
+			buf[count].ch = readb(port->membase + MCFUART_URB);
+
+			flag = TTY_NORMAL;
+			buf[count].flag = TTY_NORMAL;
+			buf[count].status = 0;
+			count++;
+	} while ((status = readb(port->membase + MCFUART_USR)) & MCFUART_USR_RXREADY);
+#if 0
+	if (status & MCFUART_USR_RXERR) {
+		writeb(MCFUART_UCR_CMDRESETERR,
+			port->membase + MCFUART_UCR);
+
+		status &= port->read_status_mask;
+
+		if (status & MCFUART_USR_RXBREAK)
+			flag = TTY_BREAK;
+		else if (status & MCFUART_USR_RXPARITY)
+			flag = TTY_PARITY;
+		else if (status & MCFUART_USR_RXFRAMING)
+			flag = TTY_FRAME;
+	}
+#endif
+	for (int i = 0 ; i < count ; i++)
+		uart_insert_char(port, buf[i].status, MCFUART_USR_RXOVERRUN, buf[i].ch, buf[i].flag);
+	
+	port->icount.rx += count;
+	trace_printk("Received %d chars on port %d\n", count, port->port_id);
+
+	tty_flip_buffer_push(&port->state->port);
+}
+#else
+
+static void mcf_rx_handler(unsigned long data)
+{
+    struct uart_port *port = (struct uart_port *)data;
+    struct mcf_uart *pp = container_of(port, struct mcf_uart, port);
+    int count = port->icount.rx;
+    u8 status, ch;
+    int char_count = 0;
+    const int max_chars = 4;
+    const u64 max_delay_ns = max_chars*100;  // 900 µs en nanosecondes
+    u64 start_time_ns = ktime_get_ns();  // Sauvegarde du temps de départ
+    bool stop = false;
+
+    do {
+        int read_chars = 0;
+
+        // Vérifier si le FIFO est plein avec le bit MCFUART_USR_RXFULL
+        if ((status = readb(port->membase + MCFUART_USR)) & MCFUART_USR_RXREADY) {
+            do {
+                ch = readb(port->membase + MCFUART_URB);
+                port->icount.rx++;
+                mcf_rx_one_char(port, status, ch);
+                read_chars++;
+                char_count++;
+
+                // Sortir si on atteint le nombre total de caractères
+                if (char_count >= max_chars) {
+                    stop = true;
+                    break;
+                }
+            } while ((status & MCFUART_USR_RXFULL) && read_chars < max_chars);
+
+            // Si le FIFO était plein, on applique un délai plus court
+            if (status & MCFUART_USR_RXFULL && !stop) {
+                udelay(40);  // Petit délai si le FIFO était plein
+            } else if (!stop) {
+                udelay(80);  // Délai normal
+            }
+        } else {
+            stop = true;  // Arrêter si aucun caractère n'est prêt à être lu
+        }
+
+        // Sortir si le délai imparti est dépassé
+        if ((ktime_get_ns() - start_time_ns) > max_delay_ns) {
+            stop = true;
+        }
+
+    } while (!stop);
+
+    trace_printk("Received %d chars on port %d\n", port->icount.rx - count, port->port_id);
+
+    tty_flip_buffer_push(&port->state->port);
+}
+
+
+#endif
 /****************************************************************************/
 
 static void mcf_tx_chars(struct mcf_uart *pp)
@@ -575,7 +830,10 @@ static int mcf_probe(struct platform_device *pdev)
 {
 	struct mcf_platform_uart *platp = dev_get_platdata(&pdev->dev);
 	struct uart_port *port;
-	int i;
+	struct mcf_uart *pp;
+	struct resource *res;
+	void __iomem *base;
+	int i, ret;
 
 	for (i = 0; ((i < MCF_MAXPORTS) && (platp[i].mapbase)); i++) {
 		port = &mcf_ports[i].port;
@@ -594,6 +852,17 @@ static int mcf_probe(struct platform_device *pdev)
 		port->rs485_config = mcf_config_rs485;
 		port->rs485_supported = mcf_rs485_supported;
 		port->has_sysrq = IS_ENABLED(CONFIG_SERIAL_MCF_CONSOLE);
+
+		pp = container_of(port, struct mcf_uart, port);
+		pp->dma_chan_rx = NULL;
+		if (i == 2) {
+			base = devm_platform_get_and_ioremap_resource(pdev, 0, &res);
+			if (IS_ERR(base)) {
+				ret = PTR_ERR(base);
+				dev_err(&pdev->dev, "Could not get resources: %d\n", ret);
+			}
+			mcf_dma_setup(port);
+		}
 
 		uart_add_one_port(&mcf_driver, port);
 	}
