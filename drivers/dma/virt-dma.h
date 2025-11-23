c4df79bc72b78b1bdc485873f1154f66b6aaf6a2
Jean-Michel Hautbois <jeanmichel.hautbois@yoseli.org>
2025-11-23 12:19:29 +0100

    dma: virt-dma: Use high-priority tasklet for completion callbacks
    Use tasklet_hi_schedule() instead of tasklet_schedule() for DMA
    completion callbacks. This improves real-time latency for DMA-driven
    applications like audio playback by ensuring DMA completion processing
    runs at higher softirq priority.
    
    Signed-off-by: Jean-Michel Hautbois <jeanmichel.hautbois@yoseli.org>

--- drivers/dma/virt-dma.h
+++ drivers/dma/virt-dma.h
@@ -106,7 +106,7 @@ static inline void vchan_cookie_complete(struct virt_dma_desc *vd)
 		 vd, cookie);
 	list_add_tail(&vd->node, &vc->desc_completed);
 
-	tasklet_schedule(&vc->task);
+	tasklet_hi_schedule(&vc->task);
 }
 
 /**
