012f6c95650e750406f70dad60622aa7298f12bf
Jean-Michel Hautbois <jeanmichel.hautbois@yoseli.org>
2025-11-24 18:14:42 +0100

    net: phy: dp83826: Add hardware strap reading and configuration
    Read the SOR1 strap register during probe and configure the PHY
    according to hardware strapping:
    
    - Bit 0: Disable autonegotiation and force 100Mbps Full Duplex
    - Bit 5: RMII slave mode (vs master mode)
    - Bit 8: Enable RMII interface mode
    - Bits 9-10: MDI-X configuration
      * Bit 9=0: Enable auto-MDIX (PHY auto-detects cable type)
      * Bit 9=1, Bit 10=0: Force MDI (straight cable)
      * Bit 9=1, Bit 10=1: Force MDIX (crossover cable)
    
    This allows non-DT platforms to configure the PHY via hardware straps
    and enables proper MDI-X reporting in ethtool.
    
    Signed-off-by: Jean-Michel Hautbois <jeanmichel.hautbois@yoseli.org>

--- drivers/net/phy/dp83822.c
+++ drivers/net/phy/dp83822.c
@@ -61,6 +61,8 @@
 /* PHYSCR Register Fields */
 #define DP83822_PHYSCR_INT_OE		BIT(0) /* Interrupt Output Enable */
 #define DP83822_PHYSCR_INTEN		BIT(1) /* Interrupt Enable */
+#define DP83822_PHYSCR_MDIX_EN		BIT(15) /* MDI/MDIX Enable */
+#define DP83822_PHYSCR_FORCE_MDIX	BIT(14) /* Force MDIX */
 
 /* MISR1 bits */
 #define DP83822_RX_ERR_HF_INT_EN	BIT(0)
@@ -208,6 +210,7 @@ struct dp83822_private {
 	bool led_pin_enable[DP83822_MAX_LED_PINS];
 	int tx_amplitude_100base_tx_index;
 	int mac_termination_index;
+	int sor1;
 };
 
 static int dp83822_config_wol(struct phy_device *phydev,
@@ -640,6 +643,7 @@ static int dp83822_config_init(struct phy_device *phydev)
 static int dp8382x_config_rmii_mode(struct phy_device *phydev)
 {
 	struct device *dev = &phydev->mdio.dev;
+	struct dp83822_private *dp83822 = phydev->priv;
 	const char *of_val;
 	int ret;
 
@@ -654,6 +658,17 @@ static int dp8382x_config_rmii_mode(struct phy_device *phydev)
 			phydev_err(phydev, "Invalid value for ti,rmii-mode property (%s)\n",
 				   of_val);
 			ret = -EINVAL;
+		}
+
+		if (ret)
+			return ret;
+	} else {
+		if (dp83822->sor1 & BIT(5)) {
+			ret = phy_set_bits_mmd(phydev, MDIO_MMD_VEND2, MII_DP83822_RCSR,
+					       DP83822_RMII_MODE_SEL);
+		} else {
+			ret = phy_clear_bits_mmd(phydev, MDIO_MMD_VEND2, MII_DP83822_RCSR,
+						 DP83822_RMII_MODE_SEL);
 		}
 
 		if (ret)
@@ -957,23 +972,71 @@ static int dp83822_read_straps(struct phy_device *phydev)
 
 static int dp83826_read_straps(struct phy_device *phydev)
 {
+	struct dp83822_private *dp83822 = phydev->priv;
 	int val;
 
 	val = phy_read_mmd(phydev, MDIO_MMD_VEND2, MII_DP83822_SOR1);
 	if (val < 0)
 		return val;
 
-	printk("STS1 strap register: 0x%04x\n", val);
+	phydev_dbg(phydev, "SOR1 strap register: 0x%04x\n", val);
 
-	val = phy_read_mmd(phydev, MDIO_MMD_VEND2, MII_DP83822_SOR2);
-	if (val < 0)
-		return val;
+	/* Bit 10: MDIX mode */
+	if (val & BIT(10))
+		phydev_dbg(phydev, "MDIX mode enabled\n");
 
-	printk("STS2 strap register: 0x%04x\n", val);
+	/* Bit 9: auto-MDIX disable */
+	if (val & BIT(9))
+		phydev_dbg(phydev, "Auto-MDIX disabled\n");
 
-	phydev->autoneg = AUTONEG_DISABLE;
-	phydev->duplex = DUPLEX_FULL;
-	phydev->speed = SPEED_100;
+	/* Bit 8: RMII */
+	if (val & BIT(8)) {
+		phydev_dbg(phydev, "RMII mode enabled\n");
+		phydev->interface = PHY_INTERFACE_MODE_RMII;
+	}
+
+	/* Bit 5: Slave mode */
+	if (val & BIT(5))
+		phydev_dbg(phydev, "RMII slave mode enabled\n");
+
+	/* Bit 0: autoneg disable */
+	if (val & BIT(0)) {
+		phydev_dbg(phydev, "Auto-negotiation disabled\n");
+		phydev->autoneg = AUTONEG_DISABLE;
+		phydev->speed = SPEED_100;
+		phydev->duplex = DUPLEX_FULL;
+	}
+
+	/* Configure MDIX based on straps */
+	if (val & BIT(9)) {
+		/* Auto-MDIX disabled - use forced mode */
+		if (val & BIT(10)) {
+			/* Force MDIX (crossover) */
+			phy_set_bits(phydev, MII_DP83822_PHYSCR,
+				     DP83822_PHYSCR_FORCE_MDIX);
+			phy_clear_bits(phydev, MII_DP83822_PHYSCR,
+				       DP83822_PHYSCR_MDIX_EN);
+			phydev->mdix_ctrl = ETH_TP_MDI_X;
+			phydev->mdix = ETH_TP_MDI_X;
+		} else {
+			/* Force MDI (straight) */
+			phy_clear_bits(phydev, MII_DP83822_PHYSCR,
+				       DP83822_PHYSCR_FORCE_MDIX |
+				       DP83822_PHYSCR_MDIX_EN);
+			phydev->mdix_ctrl = ETH_TP_MDI;
+			phydev->mdix = ETH_TP_MDI;
+		}
+	} else {
+		/* Auto-MDIX enabled */
+		phy_set_bits(phydev, MII_DP83822_PHYSCR,
+			     DP83822_PHYSCR_MDIX_EN);
+		phy_clear_bits(phydev, MII_DP83822_PHYSCR,
+			       DP83822_PHYSCR_FORCE_MDIX);
+		phydev->mdix_ctrl = ETH_TP_MDI_AUTO;
+		phydev->mdix = ETH_TP_MDI_INVALID;
+	}
+
+	dp83822->sor1 = val;
 
 	return 0;
 }
